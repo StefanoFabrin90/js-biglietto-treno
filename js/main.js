6378b77b2d09639885eb5de189ebfe7a9112cbc6
stef_fab@hotmail.it <stef_fab@hotmail.it>
2021-10-21 17:10:35 +0200

    add 7

--- js/main.js
+++ js/main.js
@@ -3,18 +3,18 @@
 
 // eta del passeggero
 const age = parseInt (prompt ('Inserisci la tua eta') );
-console.log('age');
+// console.log('age');
 
 // km da voler percorrere
 const km = parseInt ( prompt ('totale km da percorrere') );
-console.log('km');
+// console.log('km');
 
 // prezzo del biglietto
 const price = km * 0.21;
-console.log(price);
+// console.log(price);
 
 
-
+// costo biglietto con eventuali sconti
 let realPrice;
 
 if ( (age > 18) && (age < 65) ) {
@@ -24,4 +24,8 @@ if ( (age > 18) && (age < 65) ) {
 }  else if (age >= 65) {
     realPrice = price - (price * 40 / 100).toFixed(2);
 }
-console.log(realPrice);
+// console.log(realPrice);
+
+
+// html 
+document.getElementById ('risultato').innerHTML = `il costo del biglietton ammonta a euro ${realPrice}`
