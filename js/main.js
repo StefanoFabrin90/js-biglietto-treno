df55c5d8cce0956980f86ae4b694608fbb37a134
stef_fab@hotmail.it <stef_fab@hotmail.it>
2021-10-21 14:36:06 +0200

    add 1

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+const age = parseInt (prompt ('Inserisci la tua eta') );
+console.log('age');
+
+const km = parseInt ( prompt ('totale km da percorrere') );
+console.log('km');
+
+const price = km * 0.21
+console.log(price);
+
+
